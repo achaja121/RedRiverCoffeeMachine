6b2a5e1c8a0b9c584e9d9b653f1ca68b351181a9
achaja121@gmail.com <achaja121@gmail.com>
2023-10-21 19:37:20 +0100

    Initial migration

--- api/RedRiverCoffeeMachine.Api/Program.cs
+++ api/RedRiverCoffeeMachine.Api/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using RedRiverCoffeeMachine.Data.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DrinksContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Default"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ api/RedRiverCoffeeMachine.DataAccess/Migrations/20231021181649_InitialDBCreation.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RedRiverCoffeeMachine.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDBCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    RecipeStepsOrder = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drinks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Extras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Extras", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecipeSteps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StepName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeSteps", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DrinkExtra",
+                columns: table => new
+                {
+                    DrinkId = table.Column<int>(type: "int", nullable: false),
+                    ExtraId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DrinkExtra", x => new { x.DrinkId, x.ExtraId });
+                    table.ForeignKey(
+                        name: "FK_DrinkExtra_Drinks_DrinkId",
+                        column: x => x.DrinkId,
+                        principalTable: "Drinks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DrinkExtra_Extras_ExtraId",
+                        column: x => x.ExtraId,
+                        principalTable: "Extras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Drinks",
+                columns: new[] { "Id", "Name", "RecipeStepsOrder", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Lemon Tea", "1,2,3,4", 1 },
+                    { 2, "Coffee", "1,5,3,4", 2 },
+                    { 3, "Chocolate", "1,6,3", 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Extras",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Lemon" },
+                    { 2, "Sugar" },
+                    { 3, "Milk" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RecipeSteps",
+                columns: new[] { "Id", "StepName" },
+                values: new object[,]
+                {
+                    { 1, "Boil some water" },
+                    { 2, "Steep the water in the tea" },
+                    { 3, "Pour {drinkType} in the cup" },
+                    { 4, "Add {drinkExtras}" },
+                    { 5, "Brew the coffee grounds" },
+                    { 6, "Add drinking chocolate powder to the water" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "DrinkExtra",
+                columns: new[] { "DrinkId", "ExtraId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 },
+                    { 1, 3 },
+                    { 2, 2 },
+                    { 2, 3 },
+                    { 3, 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DrinkExtra_ExtraId",
+                table: "DrinkExtra",
+                column: "ExtraId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DrinkExtra");
+
+            migrationBuilder.DropTable(
+                name: "RecipeSteps");
+
+            migrationBuilder.DropTable(
+                name: "Drinks");
+
+            migrationBuilder.DropTable(
+                name: "Extras");
+        }
+    }
+}
