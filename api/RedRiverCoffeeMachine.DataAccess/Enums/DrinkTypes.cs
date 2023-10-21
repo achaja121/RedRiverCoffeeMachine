4b4ee9c581ea2fe81c5449983904b03db70dc84f
achaja121@gmail.com <achaja121@gmail.com>
2023-10-21 01:06:27 +0100

    Api project folder renamed, data access project added
    Code cleanup

---
+++ api/RedRiverCoffeeMachine.DataAccess/Enums/DrinkTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace RedRiverCoffeeMachine.Data.Enums
+{
+    public enum DrinkTypes
+    {
+        Unknown = 0,
+        Tea = 1,
+        Coffee = 2,
+        Chocolate = 3
+    }
+}
---
+++ api/RedRiverCoffeeMachine.DataAccess/Models/Drink.cs
@@ -0,0 +1,13 @@
+﻿using RedRiverCoffeeMachine.Data.Enums;
+
+namespace RedRiverCoffeeMachine.Data.Models
+{
+    public  class Drink
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DrinkTypes Type { get; set; }
+        public List<DrinkExtra> PossibleExtras { get; set; } = new List<DrinkExtra>();
+        public string RecipeStepsOrder { get; set; }
+    }
+}
